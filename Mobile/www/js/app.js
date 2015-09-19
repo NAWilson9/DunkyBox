a3804a5538d85d84ca8ea5f4e013a36065614903
Nick Wilson <nawilson9@gmail.com>
2015-09-19 15:58:27 -0500

    - Created a new ionic app and got sockets working. Suck it Chris.

--- Mobile/www/js/app.js
+++ Mobile/www/js/app.js
@@ -1,12 +1,13 @@
 // Ionic Starter App
 
+var socket;
+
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('dunkyBox', ['ionic']);
+angular.module('starter', ['ionic'])
 
-var socket;
-app.run(function($ionicPlatform,$state) {
+.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -16,19 +17,7 @@ app.run(function($ionicPlatform,$state) {
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
-    socket = io('http://10.26.41.108:1337');
-    $state.go('home');
+	socket = io('http://10.26.41.108:1337');
+	socket.emit('nickCity', 'Chris is a knoblet supreme');
   });
-});
-
-app.config(['$stateProvider',function($stateProvider){
-      $stateProvider.state('home',{
-          url:'/home',
-          controller:'HomeController',
-          templateUrl:'views/home.html'
-      })
-}]);
-
-app.controller('HomeController',['$scope','USER','$state',function($scope,USER,$state) {
-    socket.emit('nickCity', 'Chris is a knoblet supreme');
-}]);
+})
